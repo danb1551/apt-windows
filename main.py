80ca48e828c0ad11fde0eb90830e62d8a803d188
danb1551 <157605673+danb1551@users.noreply.github.com>
2025-06-05 15:24:13 +0200

    creating flask application to host all tools

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 import requests
+import json
 
-def get_list_of_tools():
-    return requests.get("apt.pythonanywhere.com")
+def get_list_of_tools(url: str = "http://127.0.0.1:5000/get-list"):
+    return json.loads(requests.get(url).content.decode("utf-8"))
+
+print(get_list_of_tools())
--- web/app.py
+++ web/app.py
@@ -1,19 +1,81 @@
 import flask
 from flask import Flask, render_template, request
+import sqlite3
+import os
 
 app = Flask(__name__)
+DATABASE = "tools.db"
+
+import os
+import sqlite3
+
+DATABASE = "tools.db"  # Změň na správnou cestu k tvé databázi
+
+def get_tools(query: str = None):
+    if not os.path.exists(DATABASE):
+        return "Databáze nebyla nalezena"
+    try:
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        if not query:
+            cursor.execute("SELECT name, url FROM tools")
+        else:
+            cursor.execute("SELECT name, url FROM tools WHERE name LIKE ?", (f"%{query}%",))
+        results = cursor.fetchall()
+        conn.close()
+        if not results:
+            return "Nenalezeny žádné nástroje."
+        return results
+    except Exception as e:
+        return f"Chyba při získávání seznamu nástrojů: {e}"
+
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html")
+
+@app.route('/generate-database')
+def generate_database():
+    if not os.path.exists(DATABASE):
+        try:
+            conn = sqlite3.connect(DATABASE)
+            cursor = conn.cursor()
+            cursor.execute("""
+                CREATE TABLE tools (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    url TEXT NOT NULL
+                );
+            """)
+            conn.commit()
+            conn.close()
+            return "databáze úspěšně vytvořena"
+        except Exception as e:
+            return e
+    else:
+        return "databáze už byla vytvořena dříve"
+
+@app.route('/add-tool', methods=['GET', 'POST'])
+def add_tool():
+    if request.method == "POST":
+        name = request.form["name"]
+        url = request.form["url"]
+        try:
+            conn = sqlite3.connect(DATABASE)
+            cursor = conn.cursor()
+            cursor.execute(f"INSERT INTO tools VALUES ({name}, {url})")
+            conn.commit()
+            conn.close()
+        except Exception as e:
+            return e
 
 @app.route('/get-list', methods=['GET', 'POST'])
 def get_list():
     if request.method == "POST":
-        data = "f"
+        data = get_tools()
         return data
     elif request.method == "GET":
-        data = "ff"
+        data = get_tools()
         return data
 
 if __name__ == "__main__":
